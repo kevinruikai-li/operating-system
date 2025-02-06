2ac48eda86e18508881a1d9e40f3ca4cddaee13a
Krasimir Kirov <kkirov@teach-node-07.cs.mcgill.ca>
2025-02-06 17:17:24 -0500

    Enhance batch mode execution

--- project/src/shell.c
+++ project/src/shell.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <unistd.h>
+#include <unistd.h>
 #include "shell.h"
 #include "interpreter.h"
 #include "shellmemory.h"
@@ -21,13 +21,20 @@ int main (int argc, char *argv[]) {
         userInput[i] = '\0';
     }
 
+    int interactive = isatty(STDIN_FILENO);
+
     //init shell memory
     mem_init ();
     while (1) {
-        printf ("%c ", prompt);
+        if (interactive) {
+            printf("%c ", prompt);
+            fflush(stdout);
+        }
         // here you should check the unistd library 
         // so that you can find a way to not display $ in the batch mode
-        fgets (userInput, MAX_USER_INPUT - 1, stdin);
+        if (fgets(userInput, MAX_USER_INPUT - 1, stdin) == NULL) {
+            break;
+        }
         errorCode = parseInput (userInput);
         if (errorCode == -1)
             exit (99);          // ignore all other errors
