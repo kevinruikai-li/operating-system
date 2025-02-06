387cf25569ad1ac1c47ab48d73aa0b98acc1ec53
Krasimir Kirov <kkirov@teach-node-07.cs.mcgill.ca>
2025-02-06 18:31:18 -0500

    Added my_mdir, my_touch and my_cd commands

--- project/src/interpreter.c
+++ project/src/interpreter.c
@@ -5,6 +5,8 @@
 #include "shell.h"
 #include <dirent.h>
 #include <ctype.h>
+#include <sys/stat.h>  
+#include <unistd.h>
 
 int MAX_ARGS_SIZE = 3;
 
@@ -25,6 +27,10 @@ int set (char *var, char *value);
 int print (char *var);
 int source (char *script);
 int echo(char *arg);
+int my_ls(void);
+int my_mkdir(char *dirname);
+int my_touch(char *filename);
+int my_cd(char *dirname);
 int badcommandFileDoesNotExist ();
 
 // Interpret commands and their arguments
@@ -71,6 +77,22 @@ int interpreter (char *command_args[], int args_size) {
         if (args_size != 2)
             return badcommand ();
         return echo(command_args[1]);
+    } else if (strcmp(command_args[0], "my_ls") == 0) {
+        if (args_size != 1)
+            return badcommand();
+        return my_ls();
+    } else if (strcmp(command_args[0], "my_mkdir") == 0) {
+        if (args_size != 2)
+            return badcommand();
+        return my_mkdir(command_args[1]);
+    } else if (strcmp(command_args[0], "my_touch") == 0) {
+        if (args_size != 2)
+            return badcommand();
+        return my_touch(command_args[1]);
+    } else if (strcmp(command_args[0], "my_cd") == 0) {
+        if (args_size != 2)
+            return badcommand();
+        return my_cd(command_args[1]);
     } else
         return badcommand ();
 }
@@ -228,5 +250,70 @@ int my_ls() {
         free(filenames[i]);
     }
     
+    return 0;
+}
+
+// Helper: returns 1 if str consists only of alphanumeric characters.
+int isAlphanumString(const char *str) {
+    if (str == NULL || *str == '\0')
+        return 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (!isalnum((unsigned char)str[i]))
+            return 0;
+    }
+    return 1;
+}
+
+int my_mkdir(char *dirname) {
+    char actualName[100];
+
+    if (dirname[0] == '$') {
+        char *varName = dirname + 1;
+        char *value = mem_get_value(varName);
+        if (value == NULL || strcmp(value, "Variable does not exist") == 0 || !isAlphanumString(value)) {
+            printf("Bad command: my_mkdir\n");
+            return 1;
+        }        
+        strncpy(actualName, value, sizeof(actualName));
+        actualName[sizeof(actualName) - 1] = '\0';
+    } else {
+        if (!isAlphanumString(dirname)) {
+            printf("Bad command: my_mkdir\n");
+            return 1;
+        }
+        strncpy(actualName, dirname, sizeof(actualName));
+        actualName[sizeof(actualName) - 1] = '\0';
+    }
+
+    if (mkdir(actualName, 0777) != 0) {
+        return 1;
+    }
+    return 0;
+}
+
+int my_touch(char *filename) {
+    if (!isAlphanumString(filename)) {
+        return 1;
+    }
+    
+    // will create the file if does not exist
+    FILE *fp = fopen(filename, "w");
+    if (fp == NULL) {
+        return 1;
+    }
+    fclose(fp);
+    return 0;
+}
+
+int my_cd(char *dirname) {
+    if (!isAlphanumString(dirname)) {
+        printf("Bad command: my_cd\n");
+        return 1;
+    }
+    
+    if (chdir(dirname) != 0) {
+        printf("Bad command: my_cd\n");
+        return 1;
+    }
     return 0;
 }
