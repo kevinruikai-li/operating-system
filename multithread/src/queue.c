e1fb06e97340024ff385f18ed4bcf3d9469b7743
Kevin Li <kevin-ruikai.li@mail.mcgill.ca>
2025-03-13 11:56:44 -0400

    Implemented Synchronized Queue

--- multithread/src/queue.c
+++ multithread/src/queue.c
@@ -6,18 +6,83 @@
 #include <pthread.h>
 
 
-struct queue *make_queue() {
-    // TODO: Allocate memory and initialize synchronization mechanisms
+struct queue *
+make_queue ()
+{
+  // Allocate memory and initialize synchronization mechanisms
+  struct queue *q = malloc (sizeof (struct queue));
+  if (!q)
+    {
+      return NULL;
+    }
+  q->head = 0;
+  q->tail = 0;
+  q->size = 0;
+  sem_init (&q->spaces, 0, 0);
+  return q;
 }
 
-void enqueue(struct queue *q, void *item) {
-    // TODO: Add item to the queue and update necessary pointers
+void
+enqueue (struct queue *q, void *item)
+{
+  // Add item to the queue and update necessary pointers
+  pthread_mutex_lock (&q->mutex);
+  struct queue_node *node = malloc (sizeof (struct queue_node));
+  if (!node)
+    {
+      fprintf (stderr, "Memory allocation error\n");
+      exit (EXIT_FAILURE);
+    }
+  node->item = item;
+  node->next = 0;
+  node->prev = q->tail;
+  if (q->tail)
+    {
+      q->tail->next = node;
+    }
+  q->tail = node;
+  if (!q->head)
+    {
+      q->head = node;
+    }
+  q->size++;
+  pthread_mutex_unlock (&q->mutex);
+  sem_post (&q->spaces);
 }
 
-void *dequeue(struct queue *q) {
-    // TODO: Retrieve an item while ensuring thread safety
+void *
+dequeue (struct queue *q)
+{
+  // Retrieve an item while ensuring thread safety
+  sem_wait (&q->spaces);
+  pthread_mutex_lock (&q->mutex);
+  struct queue_node *node = q->head;
+  q->head = node->next;
+  if (q->head)
+    {
+      q->head->prev = 0;
+    }
+  if (node == q->tail)
+    {
+      q->tail = 0;
+    }
+  q->size--;
+  pthread_mutex_unlock (&q->mutex);
+  void *item = node->item;
+  free (node);
+  return item;
 }
 
-void destroy_queue(struct queue *q) {
-    // TODO: Clean up allocated resources and synchronization mechanisms
+void
+destroy_queue (struct queue *q)
+{
+  // Clean up allocated resources and synchronization mechanisms
+  while (q->head)
+    {
+      struct queue_node *node = q->head;
+      q->head = node->next;
+      free (node);
+    }
+  sem_destroy (&q->spaces);
+  free (q);
 }
